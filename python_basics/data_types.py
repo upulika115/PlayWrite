152b9a15fbd0e6c72d5dd0d5b4399ec324d54170
upulika115 <indrajanik@gmail.com>
2025-09-25 17:00:15 +0100

    Update data_types.py

---
+++ python_basics/data_types.py
@@ -0,0 +1,12 @@
+#python has three main datatypes
+#1.Numeric ( Int, Float, Complex)
+#2.String
+#3. Complex
+
+a,b,c=1, 2.3,"Upulika"
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
