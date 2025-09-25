78189ea53f97e6cd9978a4a60e7c906280d89c0b
upulika115 <indrajanik@gmail.com>
2025-09-25 21:50:39 +0100

    add new for loop python file to present project

---
+++ python_basics/for_loop.py
@@ -0,0 +1,11 @@
+#for loop
+obj = [2,3,5,7,8,9]
+for i in obj:
+    print(i)
+
+#sum of First natural numbers 1+2+3+4+5 = 15
+#ranje(i,j) means i to j
+sum = 0
+for j in range(1,6):
+    sum = sum + j
+print(sum)
