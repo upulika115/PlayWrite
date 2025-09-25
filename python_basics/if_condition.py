911034154c634b6fc28485dbd1aaba01a7f60387
upulika115 <indrajanik@gmail.com>
2025-09-25 20:40:32 +0100

    Updated if condition python file to present project

---
+++ python_basics/if_condition.py
@@ -0,0 +1,10 @@
+greeting = "Good Morning"
+
+if greeting == "Morning":
+    print("Condition matches")
+    print("second line")
+else:
+    print("Condition doesn't match")
+
+print("if elese code is competed")
+
