d7ebe6b1dc558f080190d4ad867805630d89b162
upulika115 <indrajanik@gmail.com>
2025-09-25 15:08:44 +0100

    Add python_lessons (replacing broken python_basics submodule)

---
+++ python_lessons/first_demo.py
@@ -0,0 +1,17 @@
+print("Hello Upulika")
+#comments are added here
+
+# need not mention data type in Python
+
+a=3
+print(a)
+
+#String
+
+Str = "Upulika Katupitiya"
+print(Str)
+
+# Define multiple variables using single line
+
+b,c,d = 5,6.4, "Great"
+print(b,c,d)
