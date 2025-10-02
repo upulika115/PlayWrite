986de34e04b90035d5d4579c4d901700f69ccd1f
upulika115 <indrajanik@gmail.com>
2025-10-02 16:31:43 +0100

    add new for loop python file to present project

---
+++ PythonExercices/coding_exercise_05.py
@@ -0,0 +1,10 @@
+car = {"make": "Toyota", "model": "Camry", "year": 2020, "color": "Blue"}
+
+# Print the car model
+print("Car model:", car["model"])
+
+# Add a new key
+car["owner"] = "Rahul"
+
+# Print the updated dictionary
+print("Updated car dictionary:", car)
