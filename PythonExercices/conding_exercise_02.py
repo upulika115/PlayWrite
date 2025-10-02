96bb534e5e54e59a3fd556fdc7c7a4e3698c8ccc
upulika115 <indrajanik@gmail.com>
2025-10-02 13:07:50 +0100

    Updated if condition python file to present project

---
+++ PythonExercices/conding_exercise_02.py
@@ -0,0 +1,6 @@
+age = 20
+height = 5.9
+favorite_color = "blue"
+print("Age:", age, "|", "Type:", type(age))
+print("Height:", height, "|", "Type:", type(height))
+print("Favorite Color:", "|", favorite_color, "Type:", type(favorite_color))
