2eb08679d6623259dff164550315cdf1b17437a1
upulika115 <indrajanik@gmail.com>
2025-10-02 13:52:28 +0100

    Updated if condition python file to present project

---
+++ PythonExercices/coding_exercise_03.py
@@ -0,0 +1,5 @@
+fruits = ["apple","banana","cherry","data","elderberry"]
+
+print("{}{}".format ("First fruit: ", fruits[0]))
+print("{}{}".format("Last fruit: ", fruits[4]))
+print("{}{}".format("Fruits from index 1 to 2: ", fruits[1:3]))
