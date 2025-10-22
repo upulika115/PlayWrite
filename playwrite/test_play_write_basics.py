6e227b5a33e231520813b3609c4655ceb4b54f46
upulika115 <indrajanik@gmail.com>
2025-10-22 18:49:37 +0100

    Added Playwright test folder and basic Playwright script

--- playwrite/test_play_write_basics.py
+++ playwrite/test_play_write_basics.py
@@ -1,6 +1,12 @@
+from playwright.sync_api import Page
 def test_playwriteBasics(playwright):
    browser =  playwright.chromium.launch(headless=False)
    context = browser.new_context()
    page = context.new_page()
    page.goto("https://google.com")
+
+   #chromium headless mode 1 single context
+   def test_playwriteShortCut(page: Page):
+      page.goto("https://google.com")
+      assert "Google" in page.title()  # Optional validation
 
